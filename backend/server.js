ef5b7a3be67d05549acb3cc4e113e2c20a71ccf9
ashii-spec <ashirashaki@gmail.com>
2026-01-17 18:28:47 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,20 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+const statutoryRoutes = require("./routes/statutoryRoutes");
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/invite", require("./routes/inviteRoutes"));
+app.use("/api/statutory", statutoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Module 2 routes
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
